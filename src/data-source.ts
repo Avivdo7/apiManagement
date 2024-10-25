bd1cc535f9f36fb4422df7afcd35c65dc569864e
Avivdo7 <55305605+Avivdo7@users.noreply.github.com>
2024-10-25 17:30:57 +0300

    added .env configuration

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,17 @@ import { DataSource } from "typeorm";
 import { Organization } from "./models/organization";
 import { Endpoint } from "./models/endpoint";
 import { User } from "./models/user";
+import { config } from "dotenv";
+
+config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "6759979",
-  database: "org_management",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Organization, Endpoint, User],
   synchronize: true,
   logging: false,
